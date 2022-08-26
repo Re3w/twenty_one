0f9be4f0b856d60bfc4f11b7605c1a93691096a0
Re3w <42606834+Re3w@users.noreply.github.com>
2022-08-26 15:35:57 +0300

    Update test_items.py
    Добавление time.sleep(10) для выполнения условий

--- test_items.py
+++ test_items.py
@@ -1,3 +1,4 @@
+import time
 from selenium.webdriver.common.by import By
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
@@ -5,6 +6,6 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_search_button_add_to_cart(browser):
     browser.get(link)
-    browser.implicitly_wait(10)
+    time.sleep(30)#browser.implicitly_wait(10)
     button = browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket")
     assert button, "Искомый элемент не найден"
