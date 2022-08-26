78a91ad62cfbd40799ccb025757adf92af496a33
Aarnee <daniil.belianin@gmail.com>
2022-08-26 15:01:59 +0300

    должно работать с двумя браузерами

--- conftest.py
+++ conftest.py
@@ -1,11 +1,14 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options
 
 
 def pytest_addoption(parser):
-    parser.addoption("--browser_name", "--languages", action="store", default=None, help="Choose browser: Chrome or "
+    parser.addoption("--browser_name", action="store", default=None, help="Choose browser: Chrome or "
                                                                                          "Firefox")
+    parser.addoption('--languages', action='store', default=None,
+                     help="Choose language: ec or fr")
 
 
 @pytest.fixture(scope="function")
@@ -18,12 +21,11 @@ def browser(request):
         options = Options()
         options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
         browser = webdriver.Chrome(options=options)
-#    elif browser_name == "firefox":
-#        print("\nStrart Firefox browser for test.. ")
-#        fp = webdriver.FirefoxProfile()
-#        fp.set_preference("intl.accept_languages", user_language)
-#        browser = webdriver.Firefox(firefox_profile=fp)
-#        browser.get(link)
+    elif browser_name == "firefox":
+        print("\nStrart Firefox browser for test.. ")
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", user_language)
+        browser = webdriver.Firefox(firefox_profile=fp)
     else:
         raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
--- test_items.py
+++ test_items.py
@@ -1,11 +1,9 @@
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_search_button_add_to_cart():
-    browser = webdriver.Chrome()
+def test_search_button_add_to_cart(browser):
     browser.get(link)
     browser.implicitly_wait(10)
     button = browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket")
